21057af583c1865627de95a684fe3f7009f30b12
coder-estnefo <ofentsemasita@gmail.com>
2020-12-08 00:00:56 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { environment } from '../environments/environment.prod';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage'
 import { ShopPage } from './page/shop/shop.page';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, ShopPage],
@@ -21,7 +22,8 @@ import { ShopPage } from './page/shop/shop.page';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
             AngularFireModule.initializeApp(environment.firebaseConfig),
             AngularFirestoreModule,
-            AngularFireStorageModule
+            AngularFireStorageModule,
+            HttpClientModule
           ],
   providers: [
     StatusBar,
--- src/app/modal/order/order.page.ts
+++ src/app/modal/order/order.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CartService } from 'src/app/service/cart/cart.service';
 import { LoginService } from 'src/app/service/login/login.service';
+import { Feature, MapboxService } from 'src/app/service/mapbox/mapbox.service';
 import { OrderService } from 'src/app/service/order/order.service';
 
 @Component({
@@ -11,11 +12,28 @@ import { OrderService } from 'src/app/service/order/order.service';
 })
 export class OrderPage implements OnInit {
 
+
+  //
+  checkAddress ="";
+
+  delivery = false;
+  collect = true;
+  coordinates : any;
+  list : any;
+  selectedAddress : string= "";
+  lat;
+  lng;
+
+
+  addresses = [];
+  //
+
   constructor(
     public modalController: ModalController,
     private loginSerive: LoginService,
     public cartService: CartService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private mapboxService: MapboxService
   ) { }
 
   ngOnInit() {
@@ -32,5 +50,42 @@ export class OrderPage implements OnInit {
     const userID = this.loginSerive.getUserID();
     this.orderService.placeOrder(cartItems, userID);
   }
+
+  //
+  search(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+    if (searchTerm && searchTerm.length > 0) {
+      this.mapboxService.search_word(searchTerm)
+        .subscribe((features: Feature[]) => {
+          this.coordinates = features.map(feat => feat.geometry)
+          this.addresses = features.map(feat => feat.place_name)
+          this.list = features;
+          console.log(this.list)
+        });
+    } else {
+      this.addresses = [];
+    }
+  }
+
+  addressCheck(event){
+    this.checkAddress = event.target.value;
+    console.log("info",this.checkAddress);
+  }
+
+  onSelect(address, i) {
+    this.selectedAddress = address;
+    //  selectedcoodinates=
+  
+    console.log("lng:" + JSON.stringify(this.list[i].geometry.coordinates[0]))
+    console.log("lat:" + JSON.stringify(this.list[i].geometry.coordinates[1]))
+    this.lng = JSON.stringify(this.list[i].geometry.coordinates[0])
+    this.lat = JSON.stringify(this.list[i].geometry.coordinates[1])
+    // this.user.coords = [this.lng,this.lat];
+    console.log("index =" + i)
+    console.log(this.selectedAddress)
+    // this.user.address = this.selectedAddress;
+    this.addresses = [];
+  }
+  //
 
 }
